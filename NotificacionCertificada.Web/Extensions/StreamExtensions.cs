0a91ae35052c836bb3682d364e522f6e846e56e3
Dannover Arroyave M <dannover@gmail.com>
2024-06-01 12:46:56 -0500

    Add files via upload

---
+++ NotificacionCertificada.Web/Extensions/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace NotificacionCertificada.Web.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async Task<byte[]> ReadFully(this Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await input.CopyToAsync(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
